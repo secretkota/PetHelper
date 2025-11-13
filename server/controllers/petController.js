23d3b90fc45e9d2ee42869530c4dc3012b64a95b
secretkota <stasbro123@gmail.com>
2025-11-13 22:54:42 +0200

    FEAT: release error handler

--- server/controllers/petController.js
+++ server/controllers/petController.js
@@ -6,10 +6,6 @@ import crypto from "crypto"
 const upload = multer({ storage: multer.memoryStorage() })
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
 })
 
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import * as User from '../controllers/userController.js'
+import asyncWrapper from '../middleware/asyncWrapper.js'
 
 const router = express.Router()
 
-router.post('/register', User.Register)
-router.post('/login', User.Login)
+router.post('/register', asyncWrapper(User.Register))
+router.post('/login', asyncWrapper(User.Login))
 
 
 export default router
